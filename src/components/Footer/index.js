12978fc7bcf9313784006c67159d2f6792f7a0da
Rafael Paes Leme <rafaelspaesleme.ads@gmail.com>
2020-02-23 13:52:49 -0300

    Configuration footer of page

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Icon } from 'semantic-ui-react'
+import { Icon, Grid, Container } from 'semantic-ui-react'
 
 import './style.css';
 
@@ -8,14 +8,58 @@ const pic = [" > "," < "];
 const urlsFooters = [
                         `https://github.com/rafaelspaesleme-ads/${'my-blog'}/blob/master/README.md`,
                         `https://www.gitshowcase.com/rafaelspaesleme-ads`,
-                        `https://github.com/rafaelspaesleme-ads?utf8=%E2%9C%93&tab=repositories&q=${'rpl-lib-api-'}&type=&language=${'Java'}`
+                        `https://github.com/rafaelspaesleme-ads?utf8=%E2%9C%93&tab=repositories&q=${'rpl-lib-api-'}&type=&language=${'Java'}`,
+                        `https://instagram.com/rafaelspaesleme`,
+                        `https://twitter.com/PaesSerdeiro`,
+                        `https://aws-dev-workspace.slack.com/archives/DHFRR7BN2`
                     ];
 
 const Footer = () => (
-    <footer id="main-footer">
-        <div id="footer-left"> {pic[0]} <a target="_blank" href={urlsFooters[0]} ><Icon name="book" />Documentações</a></div>
-        <div id="footer-center">Copyright &copy; - <a href={urlsFooters[1]} target="_blank" >rafaelspaesleme-ads</a> </div>
-        <div id="footer-right"><a target="_blank" href={urlsFooters[2]} >Api's <Icon name="cogs" /></a> {pic[1]} </div>
+    <footer>
+        <Grid className="main-footer" >
+            <Grid.Column className="footer-left" >
+                <Grid.Row className="footer-cola-rowa" >
+                    
+                </Grid.Row>
+                <Grid.Row className="footer-cola-rowb" >
+                    
+                </Grid.Row>
+                <Grid.Row className="footer-cola-rowc" >
+                
+                </Grid.Row>
+                <Grid.Row className="footer-cola-rowd" >
+                    {pic[0]} <a target="_blank" href={urlsFooters[0]} ><Icon name="book" />Documentações</a>
+                </Grid.Row>
+            </Grid.Column>
+            <Grid.Column className="footer-center" >
+                <Grid.Row className="footer-colb-rowa" >
+                    
+                </Grid.Row>
+                <Grid.Row className="footer-colb-rowb" >
+                    
+                </Grid.Row>
+                <Grid.Row className="footer-colb-rowc" >
+                    
+                </Grid.Row>
+                <Grid.Row className="footer-cola-rowd" >
+                    Copyright &copy; - <a href={urlsFooters[1]} target="_blank" >rafaelspaesleme-ads</a>
+                </Grid.Row>
+            </Grid.Column>
+            <Grid.Column className="footer-right" >
+                <Grid.Row className="footer-colc-rowa" >
+                    <Icon name="instagram" /> Siga-me no <a target="_blank" href={urlsFooters[3]} >Instagram</a>
+                </Grid.Row>
+                <Grid.Row className="footer-colc-rowb" >
+                    <Icon name="twitter" /> Siga-me no <a target="_blank" href={urlsFooters[4]} >Twitter</a>
+                </Grid.Row>
+                <Grid.Row className="footer-colc-rowc" >
+                    <Icon name="slack" /> Conheça meu canal publico no <a target="_blank" href={urlsFooters[4]} >Slack</a>
+                </Grid.Row>
+                <Grid.Row className="footer-cola-rowd" >
+                    <a target="_blank" href={urlsFooters[2]} >Api's <Icon name="cogs" /></a> {pic[1]}
+                </Grid.Row>
+            </Grid.Column>
+        </Grid>
     </footer>
 )
 
