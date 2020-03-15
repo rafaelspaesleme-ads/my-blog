b2e597910fbc0c972005a412c5c7a9b15d6dd1be
Rafael Paes Leme <rafaelspaesleme.ads@gmail.com>
2020-03-15 13:20:01 -0300

    my-blog-version:dev::bug: Corrigindo endereço de ip do server local.

--- src/services/api.js
+++ src/services/api.js
@@ -1,10 +1,10 @@
 import axios from 'axios';
 
-const apiDevRpi = 'http://192.168.0.107:8081/api/blog';
-const apiDevLocal = 'localhost:8081/api/blog';
+const apiDevRpi = 'http://192.168.0.102:8081/api/blog';
+const apiDevLocal = 'http://localhost:8081/api/blog';
 const apiDevServer = 'https://myblog-api-rpl.herokuapp.com/api/blog'
 
 
-const api = axios.create({baseURL: apiDevRpi});
+const api = axios.create({baseURL: apiDevLocal});
 
 export default api;
