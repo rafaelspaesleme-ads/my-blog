86d9e1ae4ffdd1144fc5a0b37a9af93a55a3586b
Rafael Paes Leme <rafaelspaesleme.ads@gmail.com>
2020-03-30 23:06:31 -0300

    my-blog-version:dev::racehorse: Refactor routes, add pub post and article

--- src/Routes.js
+++ src/Routes.js
@@ -3,19 +3,17 @@ import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
 
 import Home from './pages/Home'
 import Login from './pages/Login'
-import Followers from './pages/inserts/Followers';
-import ProfileFollower from './pages/views/ProfileFollower'
-import Posts from './pages/inserts/Posts'
-import Article from './pages/inserts/Articles'
-import Contacts from './pages/inserts/Contacts'
-import Portfolios from './pages/inserts/Portfolios'
-import Repositories from './pages/inserts/Repositories'
 import Dashboard from './pages/views/Dashboard'
+import ShowArticle from './pages/views/ShowArticle'
+import ShowPost from './pages/views/ShowPost'
 
 import Contents from './tests/Contents'
 import { isAuthenticated } from "./services/auth";
 
 const hashPage = '12b907cab9ac73e57872cc7d6159a762'
+
+const titleArticle = localStorage.getItem('title-article');
+const titlePost = localStorage.getItem('title-post');
 
 const PrivateRoute = ({ component: Component, ...rest }) => (
     <Route
@@ -36,13 +34,8 @@ const Routes = () => (
             <Route exact path="/" component={Home} />
             <Route exact path="/home" component={Home} />
             <Route exact path="/xkj-login" component={Login} />
-            <Route exact path="/new-follower" component={Followers} />
-{/*             <Route exact path={`/create/post`} component={Posts} />
-            <Route exact path={`/create/article`} component={Article} />
-            <Route exact path={`/create/contact`} component={Contacts} />
-            <Route exact path={`/create/portfolio`} component={Portfolios} />
-            <Route exact path={`/create/repo`} component={Repositories} /> */}
-            <Route exact path={`/profile/follower/${1}`} component={ProfileFollower} />
+            <Route exact path={`/artigo/artigo-${titleArticle}`} component={ShowArticle} />
+            <Route exact path={`/post-${titlePost}`} component={ShowPost} />
             <Route exact path="/teste/content" component={Contents} />
             <PrivateRoute path="/kwlc-dashboard" component={Dashboard} />
             <Route path="*" component={() => <h1>Pagina não encontrada!</h1>} />
